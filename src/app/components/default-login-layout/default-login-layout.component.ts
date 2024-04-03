14fa1b245201c24c02f3f6813aeba4f403270d88
brunofollowill <brunodev_@outlook.com>
2024-04-03 15:20:56 -0300

    Angular

---
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-default-login-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './default-login-layout.component.html',
+  styleUrl: './default-login-layout.component.scss',
+})
+export class DefaultLoginLayoutComponent {
+  @Input() title: string = '';
+  @Input() primaryBtnText: string = '';
+  @Input() secondaryBtnText: string = '';
+  @Input() disablePrimaryBtn: boolean = true;
+  @Output('submit') onSubimit = new EventEmitter();
+  @Output('navigate') onNavigate = new EventEmitter();
+
+  submit() {
+    this.onSubimit.emit();
+  }
+
+  navigate() {
+    this.onNavigate.emit();
+  }
+}
